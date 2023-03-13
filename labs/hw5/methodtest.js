95d8b7f313272c0a7fea099134410496a2930896
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-03-12 23:55:41 -0700

    finish post and put with xhr and fetch api

--- labs/hw5/methodtest.js
+++ labs/hw5/methodtest.js
@@ -1,219 +1,233 @@
-const articleForm = document.querySelector("#articleForm");
+// Add Constants
+const form = document.querySelector("#articleForm");
 const postBtn = document.querySelector("#postBtn");
 const getBtn = document.querySelector("#getBtn");
 const putBtn = document.querySelector("#putBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
-const xmlHttp = document.querySelector("#xmlHTTP");
+const xmlHTTP = document.querySelector("#xmlHTTP");
 const fetchAPI = document.querySelector("#fetchAPI");
 const response = document.querySelector("#response");
+// Set Up the Form
+const formData = new FormData();
 
-function getCurrentDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+// Get Date and Time
+function getDate() {
+	let date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	let meridiem = hour >= 12 ? "pm" : "am";
+	hour = hour % 12 || 12;
+	let dateTime = `${month}/${day}/${year} ${hour}:${minute
+		.toString()
+		.padStart(2, "0")} ${meridiem}`;
+	return dateTime;
 }
 
-// POST
-postBtn.addEventListener("click", () => {
-  const formData = new FormData();
-  formData.append("id", articleForm.id.value);
-  formData.append("article_name", articleForm.article_name.value);
-  formData.append("article_body", articleForm.article_body.value);
-  formData.append("date", getCurrentDate());
+// Auto-fill the Date and Time when the page loads
+document.getElementById("date").value = getDate();
 
-  const url = "https://httpbin.org/post";
+// Add Event Listeners
+postBtn.addEventListener("click", postArticle);
+getBtn.addEventListener("click", getArticle);
+putBtn.addEventListener("click", putArticle);
+deleteBtn.addEventListener("click", deleteArticle);
 
-  if (xmlHttp.checked) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          const data = JSON.parse(xhr.responseText);
-          response.innerHTML = `
-						<p><strong>Form Data:</strong></p>
-						<ul>
-							<li>ID: ${data.form.id}</li>
-							<li>Article Name: ${data.form.article_name}</li>
-							<li>Article Body: ${data.form.article_body}</li>
-							<li>Date: ${data.form.date}</li>
-						</ul>
-						`;
-        } else {
-          console.log("Error: ", xhr.status);
-        }
-      }
-    };
-    xhr.send(formData);
-  } else if (fetchAPI.checked) {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: articleForm.id.value,
-        article_name: articleForm.article_name.value,
-        article_body: articleForm.article_body.value,
-        date: getCurrentDate(),
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("FETCH API POST: ", data);
-        response.innerHTML = `
-				<p><strong>Form Data:</strong></p>
-				<ul>
-					<li>ID: ${data.json.id}</li>
-					<li>Article Name: ${data.json.article_name}</li>
-					<li>Article Body: ${data.json.article_body}</li>
-					<li>Date: ${data.json.date}</li>
-				</ul>
-				`;
-      });
-  }
-});
+// Post Article
+function postArticle() {
+	const url = "https://httpbin.org/post";
+	let id = form.id.value;
+	let name = form.article_name.value;
+	let body = form.article_body.Value;
+	let date = getDate();
+	let article = {
+		id: id,
+		name: name,
+		body: body,
+		date: date,
+	};
+	if (xmlHTTP.checked) {
+		postArticleXML(article, url);
+	} else if (fetchAPI.checked) {
+		postArticleFetch(article, url);
+	}
+}
 
-// GET
-getBtn.addEventListener("click", () => {
-  const url = "https://httpbin.org/get";
+// Post Article with XML
+function postArticleXML(article, url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onload = function () {
+		if (this.status === 200) {
+			// Format the JSON response into HTML
+			response.innerHTML = `
+			<h3> Posted with XHR! </h3>
+			<h3>Article ID: ${article.id}</h3>
+			<h3>Article Name: ${article.name}</h3>
+			<h3>Article Body: ${document.getElementById("article_body").value}</h3>
+			<h3>Article Date: ${article.date}</h3>
+			`;
+		}
+	};
+	xhr.send(JSON.stringify(article));
+}
 
-  if (xmlHttp.checked) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          const data = JSON.parse(xhr.responseText);
-          console.log(JSON.parse(xhr.responseText));
-          response.innerHTML = `
-						<p><strong>URL:</strong> ${data.url}</p>
-						<p><strong>Args:</strong></p>
-						<ul>
-						<li>${data.args}</li>
-						</ul>
-						`;
-        }
-      }
-	  else {
-		console.log("Error: ", xhr.status);
-	  }
-    };
-    xhr.send();
-  } else if (fetchAPI.checked) {
-    fetch(url, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        response.innerHTML = `
-				<p><strong>URL:</strong> ${data.url}</p>
-				<p><strong>Args:</strong></p>
-				<ul>
-				<li>${data.args}</li>
-				</ul>
-				`;
-      });
-  }
-});
+// Post Article with Fetch
+function postArticleFetch(article, url) {
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify(article),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			try {
+				const article = JSON.parse(data.data);
+				let articleData = `
+			<h3> Posted with Fetch! </h3>
+		  <h3>Article ID: ${article.id}</h3>
+		  <h3>Article Name: ${article.name}</h3>
+		  <h3>Article Body: ${document.getElementById("article_body").value}</h3>
+		  <h3>Article Date: ${article.date}</h3>
+		`;
+				response.innerHTML = articleData;
+			} catch (error) {
+				console.error("Error parsing JSON:", error);
+			}
+		})
+		.catch((err) => console.log(err));
+}
 
-// PUT
-putBtn.addEventListener("click", () => {
-  const formData = new FormData();
-  formData.append("id", articleForm.id.value);
-  formData.append("article_name", articleForm.article_name.value);
-  formData.append("article_body", articleForm.article_body.value);
-  formData.append("date", getCurrentDate());
+// Get Article
+function getArticle() {
+	const url = "https://httpbin.org/get";
+	if (xmlHTTP.checked) {
+		getArticleXML(url);
+	} else if (fetchAPI.checked) {
+		getArticleFetch(url);
+	}
+}
 
-  const url = "https://httpbin.org/put";
+// Get Article with XML
+function getArticleXML(url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.onload = function () {
+		if (this.status === 200) {
+			response.innerHTML = this.responseText;
+		}
+	};
+	xhr.send();
+}
 
-  if (xmlHttp.checked) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("PUT", url);
-    xhr.send(formData);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        console.log(JSON.parse(xhr.responseText));
-        response.innerHTML = `
-						  <p><strong>Form Data:</strong></p>
-						  <ul>
-							  <li>ID: ${data.form.id}</li>
-							  <li>Article Name: ${data.form.article_name}</li>
-							  <li>Article Body: ${data.form.article_body}</li>
-							  <li>Date: ${data.form.date}</li>
-						  </ul>
-						  `;
-      }
-    };
-  } else if (fetchAPI.checked) {
-    fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: articleForm.id.value,
-        article_name: articleForm.article_name.value,
-        article_body: articleForm.article_body.value,
-        date: getCurrentDate(),
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("FETCH API PUT: ", data);
-        response.innerHTML = `
-				  <p><strong>Form Data:</strong></p>
-				  <ul>
-					  <li>ID: ${data.json.id}</li>
-					  <li>Article Name: ${data.json.article_name}</li>
-					  <li>Article Body: ${data.json.article_body}</li>
-					  <li>Date: ${data.json.date}</li>
-				  </ul>
-				  `;
-      });
-  }
-});
+// Get Article with Fetch
+function getArticleFetch(url) {
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => (response.innerHTML = data))
+		.catch((err) => console.log(err));
+}
 
-// DELETE
-deleteBtn.addEventListener("click", () => {
-  const url = "https://httpbin.org/delete";
+// Put Article
+function putArticle() {
+	const url = "https://httpbin.org/put";
+	let id = form.id.value;
+	let name = form.article_name.value;
+	let body = form.article_body.value;
+	let date = getDate();
+	let article = {
+		id: id,
+		name: name,
+		body: body,
+		date: date,
+	};
+	if (xmlHTTP.checked) {
+		putArticleXML(article, url);
+	} else if (fetchAPI.checked) {
+		putArticleFetch(article, url);
+	}
+}
 
-  if (xmlHttp.checked) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("DELETE", url);
-    xhr.send();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        console.log(JSON.parse(xhr.responseText));
-        response.innerHTML = `
-						<p><strong>URL:</strong> ${data.url}</p>
-						<p><strong>Args:</strong></p>
-						<ul>
-						<li>${data.args}</li>
-						</ul>
-						`;
-      }
-    };
-  } else if (fetchAPI.checked) {
-    fetch(url, {
-      method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        response.innerHTML = `
-				<p><strong>URL:</strong> ${data.url}</p>
-				<p><strong>Args:</strong></p>
-				<ul>
-				<li>${data.args}</li>
-				</ul>
-				`;
-      });
-  }
-});
+// Put Article with XML
+function putArticleXML(article, url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("PUT", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onload = function () {
+		if (this.status === 200) {
+			response.innerHTML = `
+			<h3> Put with XHR! </h3>
+			<h3>Article ID: ${article.id}</h3>
+			<h3>Article Name: ${article.name}</h3>
+			<h3>Article Body: ${document.getElementById("article_body").value}</h3>
+			<h3>Article Date: ${article.date}</h3>
+			`;
+		}
+	};
+	xhr.send(JSON.stringify(article));
+}
+
+// Put Article with Fetch
+function putArticleFetch(article, url) {
+	fetch(url, {
+		method: "PUT",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify(article),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			try {
+				const article = JSON.parse(data.data);
+				let articleData = `
+			<h3> Put with Fetch! </h3>
+		  <h3>Article ID: ${article.id}</h3>
+		  <h3>Article Name: ${article.name}</h3>
+		  <h3>Article Body: ${document.getElementById("article_body").value}</h3>
+		  <h3>Article Date: ${article.date}</h3>
+		`;
+				response.innerHTML = articleData;
+			} catch (error) {
+				console.error("Error parsing JSON:", error);
+			}
+		})
+		.catch((err) => console.log(err));
+}
+
+// Delete Article
+function deleteArticle() {
+	const url = "https://httpbin.org/delete";
+	if (xmlHTTP.checked) {
+		deleteArticleXML(url);
+	} else if (fetchAPI.checked) {
+		deleteArticleFetch(url);
+	}
+}
+
+// Delete Article with XML
+function deleteArticleXML(url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("DELETE", url, true);
+	xhr.onload = function () {
+		if (this.status === 200) {
+			response.innerHTML = this.responseText;
+		}
+	};
+	xhr.send();
+}
+
+// Delete Article with Fetch
+function deleteArticleFetch(url) {
+	fetch(url, {
+		method: "DELETE",
+	})
+		.then((response) => response.json())
+		.then((data) => (response.innerHTML = data))
+		.catch((err) => console.log(err));
+}
